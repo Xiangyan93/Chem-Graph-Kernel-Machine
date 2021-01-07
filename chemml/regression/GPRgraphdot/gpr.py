91da4ec4c509a7dfe082f71df3d5f15aaccdcfbf
XiangyanSJTU <hnxxxy123@sjtu.edu.com>
2021-01-07 21:57:29 +0800

    bugfix

--- chemml/regression/GPRgraphdot/gpr.py
+++ chemml/regression/GPRgraphdot/gpr.py
@@ -7,6 +7,46 @@ from graphdot.model.gaussian_process.gpr import GaussianProcessRegressor
 
 
 class GPR(GaussianProcessRegressor):
+    def predict_(self, Z, return_std=False, return_cov=False):
+        """Predict using the trained GPR model.
+
+        Parameters
+        ----------
+        Z: list of objects or feature vectors.
+            Input values of the unknown data.
+        return_std: boolean
+            If True, the standard-deviations of the predictions at the query
+            points are returned along with the mean.
+        return_cov: boolean
+            If True, the covariance of the predictions at the query points are
+            returned along with the mean.
+
+        Returns
+        -------
+        ymean: 1D array
+            Mean of the predictive distribution at query points.
+        std: 1D array
+            Standard deviation of the predictive distribution at query points.
+        cov: 2D matrix
+            Covariance of the predictive distribution at query points.
+        """
+        if not hasattr(self, 'Kinv'):
+            raise RuntimeError('Model not trained.')
+        Ks = self._gramian(Z, self.X)
+        ymean = (Ks @ self.Ky) * self.y_std + self.y_mean
+        if return_std is True:
+            Kss = self._gramian(Z, diag=True)
+            std = np.sqrt(
+                np.maximum(0, Kss - (Ks @ (self.Kinv @ Ks.T)).diagonal())
+            )
+            return (ymean, std)
+        elif return_cov is True:
+            Kss = self._gramian(Z)
+            cov = np.maximum(0, Kss - Ks @ (self.Kinv @ Ks.T))
+            return (ymean, cov)
+        else:
+            return ymean
+
     def predict(self, X, return_std=False, return_cov=False, memory_save=True,
                 n_memory_save=1000):
         if return_cov or not memory_save:
@@ -19,11 +59,11 @@ class GPR(GaussianProcessRegressor):
             for i in range(math.ceil(N / n_memory_save)):
                 X_ = X[i * n_memory_save:(i + 1) * n_memory_save]
                 if return_std:
-                    y_mean_, y_std_ = super().predict(
+                    [y_mean_, y_std_] = self.predict_(
                         X_, return_std=return_std, return_cov=return_cov)
                     y_std = np.r_[y_std, y_std_]
                 else:
-                    y_mean_ = super().predict(
+                    y_mean_ = self.predict_(
                         X_, return_std=return_std, return_cov=return_cov)
                 y_mean = np.r_[y_mean, y_mean_]
             if return_std:
