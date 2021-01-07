81a4c0aa20e9fe0ef5ab44cef63080b34c311f81
XiangyanSJTU <hnxxxy123@sjtu.edu.com>
2021-01-07 10:47:24 +0800

    memory save for GPR predict.

--- chemml/regression/GPRgraphdot/gpr.py
+++ chemml/regression/GPRgraphdot/gpr.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
 import pickle
+import math
+import numpy as np
 from graphdot.model.gaussian_process.gpr import GaussianProcessRegressor
 
 
 class GPR(GaussianProcessRegressor):
+    def predict(self, X, return_std=False, return_cov=False, memory_save=True):
+        if return_cov or not memory_save:
+            return super().predict(X, return_std=return_std,
+                                 return_cov=return_cov)
+        else:
+            N = X.shape[0]
+            y_mean = np.array([])
+            y_std = np.array([])
+            for i in range(math.ceil(N / 1000)):
+                X_ = X[i * 1000:(i + 1) * 1000]
+                if return_std:
+                    y_mean_, y_std_ = super().predict(
+                        X_, return_std=return_std, return_cov=return_cov)
+                    y_std = np.r_[y_std, y_std_]
+                else:
+                    y_mean_ = super().predict(
+                        X_, return_std=return_std, return_cov=return_cov)
+                y_mean = np.r_[y_mean, y_mean_]
+            if return_std:
+                return y_mean, y_std
+            else:
+                return y_mean
+
     @classmethod
     def load_cls(cls, f_model, kernel):
         store_dict = pickle.load(open(f_model, 'rb'))
